b8d630eb3feb51f06b52b7da664f0e7a870f343d
Calen-git <calen.nordstrom@gmail.com>
2024-11-04 10:08:27 -0700

    added more buttons, and a hot dog.

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-function buttonscript() {
-  console.log("button clicked!");
+function buttonscript1() {
+  console.log("button 1 clicked!");
+  var x = document.getElementById("button-gif");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function buttonscript2() {
+  console.log("button 2 clicked!");
   var x = document.getElementById("button-image");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
+}
+
+function buttonscript3() {
+  console.log("button 3 clicked!");
+  document.body.style.backgroundColor = "red";
+}
+
+function buttonscript4() {
+  console.log("button 4 clicked!");
+  document.body.style.backgroundColor = "orange";
+}
+
+function buttonscript5() {
+  console.log("button 5 clicked!");
+  document.body.style.backgroundColor = "yellow";
+}
+
+function buttonscript6() {
+  console.log("button 6 clicked!");
+  document.body.style.backgroundColor = "green";
+}
+
+function buttonscript7() {
+  console.log("button 7 clicked!");
+  document.body.style.backgroundColor = "blue";
 }
